0950ba5721b470d403ffee877e62c390c827e6c6
alex-kas <alex-kas@altaray.net>
2015-08-27 00:20:20 +0200

    Version 0.9

--- recovery/adbtestgpt.sh
+++ recovery/adbtestgpt.sh
@@ -1,5 +1,9 @@
 #!/bin/bash
 adb push run_root_shell /data/local/tmp
 adb push sgdisk /data/local/tmp
-adb shell '/data/local/tmp/run_root_shell -c "sgdisk -p /dev/block/mmcblk0"'
+
+adb shell chmod 755 /data/local/tmp/run_root_shell
+adb shell chmod 755 /data/local/tmp/sgdisk
+
+adb shell /data/local/tmp/run_root_shell -c "/data/local/tmp/sgdisk -p /dev/block/mmcblk0"
 exit 0
